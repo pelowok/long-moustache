9ce5c83c0f1fd1cce6a121a98cd80948deb63e32
pelowok <zak@mynameiszak.com>
2015-10-08 14:54:15 -0400

    branch2 from home

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,43 +6,25 @@ CARDS.Main = function() {
     var pub = {};
     var barData = [];
 
-    var barDataMax;
-    var barDataMin;
-
     function init() {
 
-        d3.json('scripts/testdata.json', function (error, json) {
+        datachange('scripts/testdata.json');
 
-            if (error) return console.warn(error);
-            barData = json;
+    }
 
-            setMinMax(barData);
+    function tracedata(barData){
+        console.log(barData);
+        pub.barData = barData;
+    }
 
-        });
+    function datachange(src){
 
-    })
+        d3.json(src, tracedata);
 
-    function setMinMax(myData){
-
-        myData.forEach(function(d, i) {
-
-            barDataMax = d3.max(d.values);
-            barDataMin = d3.min(d.values);
-
-            console.log(myData);
-            console.log(barDataMax);
-            console.log(barDataMin);
-
-        })
-
-    };
+    }
 
     pub.init = init();
-    pub.setMinMax = setMinMax(["index", "wut"]);
-
     pub.barData = barData;
-    pub.barDataMin = barDataMax;
-    pub.barDataMin = barDataMin;
 
     return pub;
 
@@ -50,5 +32,4 @@ CARDS.Main = function() {
 
 ( document.ready = function() {
     CARDS.Main.init;
-
 });
